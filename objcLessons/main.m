cd0f6ca3d8571c3e1bfe5b1950d4e237f6604571
Alexander Mink <alexander.mink1@gmail.com>
2020-10-14 11:09:17 +0300

    template

---
+++ objcLessons/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  objcLessons
+//
+//  Created by Александр Минк on 14.10.2020.
+//  Copyright © 2020 Alexander Mink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+        
+//        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
